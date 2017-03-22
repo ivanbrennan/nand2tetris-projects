fef6c2980d9c1c6efd8c7a38c1d79a5e55a104b0
ivanbrennan <ivan.brennan@gmail.com>
2017-03-21 21:47:17 -0400

    04: Fill

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,64 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// screenword = SCREEN
+      @SCREEN
+      D=A
+      @screenword
+      M=D
+
+// maxword = SCREEN + 8192
+      @SCREEN
+      D=A
+      @maxword
+      M=D
+      @8192
+      D=A-1
+      @maxword
+      M=M+D
+
+// while true
+(LOOP)
+      @KBD   // if keyboard == 0
+      D=M
+      @WHITE // draw white
+      D;JEQ
+      @BLACK // else draw black
+      0;JEQ
+
+(WHITE)
+      @screenword
+      A=M
+      M=0
+      @INCREMENT
+      0;JEQ
+
+(BLACK)
+      @screenword
+      A=M
+      M=-1
+      @INCREMENT
+      0;JEQ
+
+(INCREMENT)
+      @maxword
+      D=M
+      @screenword
+      D=D-M
+      @RESET
+      D;JEQ // reset if maxword - screenword == 0
+
+      @screenword
+      M=M+1 // screenword += 1
+
+      @LOOP
+      0;JEQ
+
+(RESET)
+      @SCREEN
+      D=A
+      @screenword
+      M=D
+
+      @LOOP
+      0;JEQ
