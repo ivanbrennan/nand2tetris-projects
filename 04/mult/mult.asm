f8e3f1e7af5821615276ef3d8e35f71fe72457b7
ivanbrennan <ivan.brennan@gmail.com>
2017-03-21 20:27:24 -0400

    04: Mult

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -6,4 +6,30 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// R2 = 0
+      @R2
+      M=0
+
+// while R1 > 0
+(LOOP)
+      @R1
+      D=M
+      @END
+      D;JEQ
+
+//   R2 += R0
+      @R0
+      D=M
+      @R2
+      M=D+M
+
+//   R1 -= 1
+      @R1
+      M=M-1
+
+      @LOOP
+      0;JEQ
+
+(END)
+      @END
+      0;JMP
